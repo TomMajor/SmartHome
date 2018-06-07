86ca12cb3484e67e4612e4f097570c5c9ff8310a
TomMajor <fake34zg42hwqf@byom.de>
2018-06-07 19:57:35 +0200

    Minor updates for CCU/RM scripts and TSL2561 comments

--- HB-UNI-Sensor1/Sensors/Sens_Tsl2561.h
+++ HB-UNI-Sensor1/Sensors/Sens_Tsl2561.h
@@ -60,10 +60,13 @@ public:
       uint8_t overflow = 0;
       do {
         measureRaw(sensitivity);
-	// zu viel Licht
+	// default wird mit Gain 0 und 402ms Messzeit gemessen
+	// bei zu viel Licht die Empfindlichkeit (exposure time) solange verringern bis sinnvolle Werte kommen
+	// momentan wird hier nur mit Gain 0 gearbeitet, Gain 16 ist fuer Aussenhelligkeiten problematisch (siehe Messwerte) und wuerde nur etwas bringen
+	// falls man in sehr dunklen Umgebungen Lux-Werte mit hoher Aufloesung braucht
         if ( (_brightnessFull == _brightnessIR) || (_brightnessFull > 60000) ) {
           if (sensitivity > 0) { sensitivity--; }
-	  else { overflow = 1; break; }	// ungültiger Wert, keine weitere Sensitivity Verringerung möglich
+	  else { overflow = 1; break; }	// ungueltiger Wert, keine weitere Verringerung der Empfindlichkeit moeglich
         }
 	else { break; }			// brightness Wert ok
       } while (true);
@@ -71,7 +74,7 @@ public:
         _brightnessLux  = _tsl2561.calculateLux(_brightnessFull, _brightnessIR);
       }
       else {
-        _brightnessLux  = 130000;	// overflow, zu viel Licht, besser 130000 Lux (klarer Himmel und Sonne im Zenit) anstatt 0 Lux melden!
+        _brightnessLux  = 130000;	// overflow, zu viel Licht, besser dem SmartHome System 130000 Lux (klarer Himmel und Sonne im Zenit) anstatt 0 Lux melden!
       }
       
       DPRINT("TSL2561 Sensitivity    : "); DDECLN(sensitivity);
@@ -94,8 +97,9 @@ public:
 
 /*
 
-Innen
+Innenraum
 -------------------------------------------------------------------------------
+										Faktor	sensitivity
 Time  13  Gain  0  Lum 00060015  Full    21  IR     6  Vis    15  Lux   192	1	0
 Time 101  Gain  0  Lum 002F009C  Full   156  IR    47  Vis   109  Lux   186	7.8	1
 Time 402  Gain  0  Lum 00BC026F  Full   623  IR   188  Vis   435  Lux   186	31	2
@@ -103,7 +107,7 @@ Time  13  Gain 16  Lum 0064014B  Full   331  IR   100  Vis   231  Lux   181	16	-
 Time 101  Gain 16  Lum 02EF09BA  Full  2490  IR   751  Vis  1739  Lux   185	125	-
 Time 402  Gain 16  Lum 0BB826D0  Full  9936  IR  3000  Vis  6936  Lux   186	500	-
 
-Sonne, Juni 18 Uhr, indirekt
+Sonne, Juni, 18 Uhr, Sensor im Halbschatten
 -------------------------------------------------------------------------------
 Time  13  Gain  0  Lum 025305EF  Full  1519  IR   595  Vis   924  Lux  9713
 Time 101  Gain  0  Lum 114D2C25  Full 11301  IR  4429  Vis  6872  Lux  9805
@@ -112,7 +116,7 @@ Time  13  Gain 16  Lum 13441344  Full  4932  IR  4932  Vis     0  Lux    53	inva
 Time 101  Gain 16  Lum 90C690C6  Full 37062  IR 37062  Vis     0  Lux    54	invalid
 Time 402  Gain 16  Lum FFFFFFFF  Full 65535  IR 65535  Vis     0  Lux    24	invalid
 
-Sonne, Juni 18 Uhr, direkt
+Sonne, Juni, 18 Uhr, Sensor direkt in die Sonne ausgerichtet
 -------------------------------------------------------------------------------
 Time  13  Gain  0  Lum 13441344  Full  4932  IR  4932  Vis     0  Lux   846	invalid
 Time 101  Gain  0  Lum 90C690C6  Full 37062  IR 37062  Vis     0  Lux   863	invalid
