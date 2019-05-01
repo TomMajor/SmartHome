d71b4934a63e73544e2e4b412168da8ca8a85154
TomMajor <fake34zg42hwqf@byom.de>
2019-05-01 18:12:56 +0200

    Unified CCU/RM AddOn install files for HB-UNI-Sensor1

--- HB-UNI-Sensor1/CCU_RM/build.sh
+++ HB-UNI-Sensor1/CCU_RM/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source ./src/addon/params 
 rm ${DEVICE}-addon.tgz
-find . -name ".DS_Store" -exec rm -rf {} \;
+#find . -name ".DS_Store" -exec rm -rf {} \;
 cd src
 echo ${ADDON_VERSION} > ./addon/VERSION
 
@@ -13,13 +13,15 @@ echo "set downloadURL \"${DOWNLOAD_URL}\"" >> ./addon/update-check.cgi
 cat ../update-check.cgi.skel >> ./addon/update-check.cgi
 fi 
 
-find ./rc.d/ -type f -exec mv {} ./rc.d/${ADDON_NAME} \;
+#find ./rc.d/ -type f -exec mv {} ./rc.d/${ADDON_NAME} \;
         
 chmod +x update_script
 chmod +x addon/install*
 chmod +x addon/uninstall*
 chmod +x addon/update-check.cgi
 chmod +x addon/functions
+#chmod +x addon/extra
 chmod +x rc.d/*
+
 tar -zcvf ../${DEVICE}-addon.tgz *
 cd ..
