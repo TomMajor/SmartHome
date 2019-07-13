f63d88d31a761aa0b50d66e9f3047d27c1a787a3
TomMajor <fake34zg42hwqf@byom.de>
2019-07-14 00:54:25 +0200

    HB-UNI-Sensor1, change BME280 filter settings

--- HB-UNI-Sensor1/Sensors/Sens_BME280.h
+++ HB-UNI-Sensor1/Sensors/Sens_BME280.h
@@ -1,8 +1,8 @@
 
 //---------------------------------------------------------
 // Sens_BME280
-// 2018-08-12 Tom Major (Creative Commons)
-// https://creativecommons.org/licenses/by-nc-sa/3.0/
+// 2019-07-14 Tom Major (Creative Commons)
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
 // You are free to Share & Adapt under the following terms:
 // Give Credit, NonCommercial, ShareAlike
 // +++
@@ -19,6 +19,15 @@
 
 namespace as {
 
+BME280I2C::Settings settings(BME280::OSR_X1,                // Temperature Oversampling Rate (tempOSR): OSR Enum, default = OSR_X1
+                             BME280::OSR_X1,                // Humidity Oversampling Rate (humOSR): OSR Enum, default = OSR_X1
+                             BME280::OSR_X1,                // Pressure Oversampling Rate (presOSR): OSR Enum, default = OSR_X1
+                             BME280::Mode_Forced,           // Mode (mode): Mode Enum, default = Mode_Forced
+                             BME280::StandbyTime_1000ms,    // Standby Time (standbyTime): StandbyTime Enum, default = StandbyTime_1000ms
+                             BME280::Filter_Off,            // Filter (filter): Filter Enum, default = Filter_16
+                             BME280::SpiEnable_False        // SPI Enable: SpiEnable Enum, default = false
+);
+
 class Sens_BME280 : public Sensor {
 
     int16_t  _temperature;
@@ -26,14 +35,14 @@ class Sens_BME280 : public Sensor {
     uint16_t _pressureNN;
     uint8_t  _humidity;
     BME280I2C
-    _bme280;    // Default : forced mode, standby time = 1000 ms, Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off
+    _bme280;
 
     void measureRaw(uint16_t altitude)
     {
         float temp(NAN), hum(NAN), pres(NAN);
-        _bme280.read(pres, temp, hum, BME280::TempUnit_Celsius, BME280::PresUnit_hPa); // hPa
-        _temperature = (int16_t)(temp * 10); // HB-UNI-Sensor1: C*10
-        _pressure    = (uint16_t)(pres * 10); // HB-UNI-Sensor1: hPa*10
+        _bme280.read(pres, temp, hum, BME280::TempUnit_Celsius, BME280::PresUnit_hPa);    // hPa
+        _temperature = (int16_t)(temp * 10);                                              // HB-UNI-Sensor1: C*10
+        _pressure    = (uint16_t)(pres * 10);                                             // HB-UNI-Sensor1: hPa*10
         _pressureNN  = (uint16_t)(EnvironmentCalculations::EquivalentSeaLevelPressure(float(altitude), temp, pres) * 10);
         _humidity    = (uint8_t)hum;
     }
@@ -60,11 +69,13 @@ public:
         switch (_bme280.chipModel()) {
         case BME280::ChipModel_BME280:
             _present = true;
+            _bme280.setSettings(settings);
             DPRINTLN(F("BME280 found"));
             measureRaw(0);    // dummy read da die Messwerte der ersten Messung nach power-on weit daneben liegen
             break;
         case BME280::ChipModel_BMP280:
             _present = true;
+            _bme280.setSettings(settings);
             DPRINTLN(F("BMP280 found (No Humidity available)"));
             measureRaw(0);
             break;
