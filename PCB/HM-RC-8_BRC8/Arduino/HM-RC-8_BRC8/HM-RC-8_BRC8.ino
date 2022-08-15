c3df08c6cd066f17e67c869bf690e2279f2be9e1
TomMajor <fake34zg42hwqf@byom.de>
2022-08-15 23:10:46 +0200

    HM-RC-8_BRC8, add some documentation

---
+++ PCB/HM-RC-8_BRC8/Arduino/HM-RC-8_BRC8/HM-RC-8_BRC8.ino
@@ -0,0 +1,128 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2016-10-31 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+// 2020-12-26 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+// ci-test=yes board=328p aes=no
+
+// define this to read the device id, serial and device type from bootloader section
+// #define USE_OTA_BOOTLOADER
+#define SENSOR_ONLY
+#define SIMPLE_CC1101_INIT
+#define NOCRC
+#define NDEBUG
+
+#define STORAGEDRIVER at24cX<0x50,128,32> 
+#include <Wire.h>
+
+#define EI_NOTEXTERNAL
+#include <EnableInterrupt.h>
+#include <AskSinPP.h>
+#include <LowPower.h>
+//#include <ResetOnBoot.h>
+
+#include <MultiChannelDevice.h>
+#include <Remote.h>
+
+#define LED         8
+#define CC1101_GDO0 2
+#define CC1101_PWR  5
+#define BTN01_PIN   0
+#define BTN02_PIN   14
+#define BTN03_PIN   15
+#define BTN04_PIN   16
+#define BTN05_PIN   17
+#define BTN06_PIN   4
+#define BTN07_PIN   6
+#define BTN08_PIN   7
+
+#define SLEEP_DELAY_MS    3000 //wait milliseconds before sleep mode
+#define PEERS_PER_CHANNEL 10
+
+// all library classes are placed in the namespace 'as'
+using namespace as;
+
+// define all device properties
+const struct DeviceInfo PROGMEM devinfo = {
+    {0xA5,0xA9,0x01},       // Device ID
+    "JPBRC80001",           // Device Serial
+    {0x00,0xda},            // Device Model
+    0x10,                   // Firmware Version
+    as::DeviceType::Remote, // Device Type
+    {0x00,0x00}             // Info Bytes
+};
+
+/**
+ * Configure the used hardware
+ */
+typedef AvrSPI<10,11,12,13> SPIType;
+typedef Radio<SPIType,CC1101_GDO0, CC1101_PWR> RadioType;
+typedef StatusLed<LED> LedType;
+typedef AskSin<LedType,IrqInternalBatt,RadioType> Hal;
+
+typedef RemoteChannel<Hal,PEERS_PER_CHANNEL,List0> ChannelType;
+typedef MultiChannelDevice<Hal,ChannelType,8> RemoteType;
+
+Hal hal;
+RemoteType sdev(devinfo,0x20);
+//ResetOnBoot<RemoteType> resetOnBoot(sdev);
+
+void setup () {
+  DINIT(57600,ASKSIN_PLUS_PLUS_IDENTIFIER);
+  sdev.init(hal);
+  hal.battery.init();
+  hal.battery.low(23);
+  hal.battery.critical(20);
+
+  remoteISR(sdev,1,BTN01_PIN);
+  remoteISR(sdev,2,BTN02_PIN);
+  remoteISR(sdev,3,BTN03_PIN);
+  remoteISR(sdev,4,BTN04_PIN);
+  remoteISR(sdev,5,BTN05_PIN);
+  remoteISR(sdev,6,BTN06_PIN);
+  remoteISR(sdev,7,BTN07_PIN);
+  remoteISR(sdev,8,BTN08_PIN);
+
+  while (hal.battery.current() == 0);
+
+ // resetOnBoot.init();
+  sdev.initDone();
+  sdev.startPairing();
+}
+
+class PowerOffAlarm : public Alarm {
+  private:
+    bool    timerActive;
+  public:
+    PowerOffAlarm () : Alarm(0), timerActive(false) {}
+    virtual ~PowerOffAlarm () {}
+
+    void activateTimer(bool en) {
+      if (en == true && timerActive == false) {
+        sysclock.cancel(*this);
+        set(millis2ticks(SLEEP_DELAY_MS));
+        sysclock.add(*this);
+      } else if (en == false) {
+        sysclock.cancel(*this);
+      }
+      timerActive = en;
+    }
+
+    virtual void trigger(__attribute__((unused)) AlarmClock& clock) {
+      powerOff();
+    }
+
+    void powerOff() {
+      hal.led.ledOff();
+      hal.radio.setIdle();
+      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    }
+
+} pwrOffAlarm;
+
+void loop() {
+  bool worked = hal.runready();
+  bool poll = sdev.pollRadio();
+  pwrOffAlarm.activateTimer( hal.activity.stayAwake() == false &&  worked == false && poll == false );
+
+}
---
+++ PCB/HM-RC-8_BRC8/README.md
@@ -0,0 +1,52 @@
+
+# RWE/Innogy/Livisi Fernbedienung BRC8
+
+- habe das Projekt jetzt mal nachgebaut, funktioniert super, vielen Dank an die Ersteller des Projekts. :)
+
+- da ich im Thread von beschädigten Leiterplatten-Pads beim An-/Ablöten der ISP-Verbindung gelesen hatte, habe ich eine andere Variante probiert und eine 2mm Buchsenleiste von Reichelt auf die ISP Pins gelötet, die verbleibt dann dort dauerhaft.
+
+- von der Bauhöhe passt die gut rein, man muss nur einen der 4 Halter für die kleine Drahtantenne entfernen, im 3. Bild ist das zu sehen.
+
+- so kann man jederzeit programmieren ohne sich um die Pads Gedanken zu machen. War bei mir schon hilfreich da ich bereits ein 2. mal mit ISP ran musste.
+
+- und dann habe ich gleich noch einen Adapter Standard AVR-ISP auf 2mm RWE einreihig gebaut, ggf. auch für weitere RWE/Innogy/Livisi Geräte.
+
+- Reichelt Buchsenleiste: z.B. BL 1X10G 2,00
+
+- Autor des HM-RC-8_BRC8 Arduino Sketches: [jp112sdl](https://github.com/jp112sdl/Beispiel_AskSinPP/tree/master/examples/RWE/HM-RC-8_BRC8).
+
+
+## Bilder
+
+![pic](Images/HM-RC-8_BRC8_1.jpg)
+
+![pic](Images/HM-RC-8_BRC8_2.jpg)
+
+![pic](Images/HM-RC-8_BRC8_3.jpg)
+
+![pic](Images/HM-RC-8_BRC8_4.jpg)
+
+![pic](Images/HM-RC-8_BRC8_5.jpg)
+
+![pic](Images/HM-RC-8_BRC8_Adapter1.jpg)
+
+![pic](Images/HM-RC-8_BRC8_Adapter2.jpg)
+
+![pic](Images/HM-RC-8_BRC8_Adapter3.jpg)
+
+![pic](Images/HM-RC-8_BRC8_Fuses_original.png)
+
+![pic](Images/HM-RC-8_BRC8_Fuses_changed.png)
+
+
+## Links
+
+[HomeMatic Forum: RWE/Innogy/Livisi Fernbedienung BRC8 - Homematic/AskSinPP Firmware](https://homematic-forum.de/forum/viewtopic.php?f=76&t=64100)
+
+
+## Lizenz
+
+**Creative Commons BY-NC-SA**<br>
+Give Credit, NonCommercial, ShareAlike
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
